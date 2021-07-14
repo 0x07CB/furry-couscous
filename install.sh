07666ec8a72f9e646ea1ae554eb64067d8597847
0x07CB <83157348+0x07CB@users.noreply.github.com>
2021-07-15 01:27:33 +0200

    add first codes

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo cp couscous.py /usr/bin/couscous
+sudo chmod a+x /usr/bin/couscous
+echo "If no error message, then is good !"
+echo "Try with 'couscous --help' first okay ? Take care it's dangerous !"
